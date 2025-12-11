e30f46484de7404cabe8c7aded9796524cf17953
Loan <aubryloan74@gmail.com>
2025-12-11 14:20:13 +0100

    Cleaned update_quality + added Conjured item type

--- gilded_rose.py
+++ gilded_rose.py
@@ -7,34 +7,62 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
+
+            # Sulfuras
+            if item.name == "Sulfuras, Hand of Ragnaros":
+                continue
+
+            # Brie
+            elif item.name == "Aged Brie":
                 if item.quality < 50:
                     item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
+                
                 item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
+                
+                if item.sell_in < 0:
                     if item.quality < 50:
                         item.quality = item.quality + 1
 
+            # Backstage pass
+            elif item.name.startswith("Backstage passes"):
+                if item.quality < 50:
+                    item.quality = item.quality + 1
+                if item.sell_in < 11:
+                    if item.quality < 50:
+                        item.quality = item.quality + 1
+                if item.sell_in < 6:
+                    if item.quality < 50:
+                        item.quality = item.quality + 1
+                
+                item.sell_in = item.sell_in - 1
+                
+                if item.sell_in < 0:
+                    item.quality = 0
+
+            elif item.name.startswith("Conjured"):
+                if item.quality > 0:
+                    item.quality = item.quality - 2
+                
+                item.sell_in = item.sell_in - 1
+                
+                if item.sell_in < 0:
+                    if item.quality > 0:
+                        item.quality = item.quality - 2
+                
+                # Sécurité : La qualité ne doit pas être négative
+                if item.quality < 0:
+                    item.quality = 0
+
+            # Normal items
+            else:
+                if item.quality > 0:
+                    item.quality = item.quality - 1
+                
+                item.sell_in = item.sell_in - 1
+                
+                if item.sell_in < 0:
+                    if item.quality > 0:
+                        item.quality = item.quality - 1
 
 class Item:
     def __init__(self, name, sell_in, quality):
