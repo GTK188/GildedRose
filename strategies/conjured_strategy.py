ca32e8d4f3bbcec5d0f6f1c1c34b4d0466331121
Loan <aubryloan74@gmail.com>
2025-12-11 15:07:45 +0100

    removed useless code

--- strategies/conjured_strategy.py
+++ strategies/conjured_strategy.py
@@ -1,5 +1,4 @@
 from strategies.normal_strategy import NormalItemStrategy
-from strategies.constants import MIN_QUALITY
 
 class ConjuredStrategy(NormalItemStrategy):
     def decrease_quality(self, amount: int = 1) -> None:
